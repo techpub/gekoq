e2a9864384dfa08b56675244b1288b50a4f0b7f4
Jason Mendes <jason@honua.co>
2015-11-13 15:00:08 -1000

    Use vanilla https module instead of request; ES6
    Yay! Request was nice in the beginning to get this kickstarted, as I
    was already familiar with its API, though I find it to be quite a
    'heavy' module, especially for this use case where it's very
    straightforward what needs to be done, so I am happy to have swapped it
    out for Node's plain https module. In fact, it's nearly just as much
    'work' to implement in the `request` module than with Node's https.request.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-var api_key = '';
-var push  = require('./push.js');
+'use strict';
+const https = require('https');
 
-module.exports.key = function (key) {
-  api_key = key;  
+let api_key = '';
+
+function post(key, widget) {
+  let payload = { api_key: key, data: widget.data };
+  https.request({
+    hostname: 'push.geckoboard.com', 
+    port: 443, 
+    path: '/v1/send/' + widget.id,
+    method: 'POST', 
+    headers: { 
+      'Content-Type': 'application/json', 
+      'Content-Length': JSON.stringify(payload).length 
+    }
+  }, (res) => {
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(widget.id + ' => ' + chunk);
+    });
+    res.on('error', (e) => { console.log('Error: ' + e) });
+  }).write(JSON.stringify(payload));
 }
 
 module.exports.push = function (widget) {
-  return push(api_key, widget);
+  return post(api_key, widget);
+}
+
+module.exports.key = function (key) {
+  api_key = key;  
 }
